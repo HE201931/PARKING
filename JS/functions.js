f70af37a9e12ba4e4066c0d8d6d3ef49794214cd
HE201931 <71380301+HE201931@users.noreply.github.com>
2020-10-18 15:31:17 +0200

    Add files via upload
    Added file without static members
    Fixes (missing ";" , not working function)

--- JS/functions.js
+++ JS/functions.js
@@ -7,24 +7,32 @@ class arrayHelper{
     {
         for(let i = 0 ; i <= globalArray.length - 1 ; i++)
         {
-
             globalArray[i] = new Array(3);
-
           for(let o = 0 ; o <= globalArray[i].length - 1 ; o++)
           {
               globalArray[i][o] = "";
           }
-
         }
     }
+
+    static resetArray()
+    {
+        globalArray = new Array(70);
+    }
+
     static beginWithThisFunction()
     {
-        if( globalArray.every( elem => elem == undefined) == true){arrayHelper.initializeArray();}
+        if(globalArray.every( elem => elem == undefined) == true){arrayHelper.initializeArray();}
 
-            let testValue = arrayHelper.encoderVehicle();
+/**
+        console.log(testValue[0]);
+        console.log(testValue[1]);
+        console.log(testValue[2]);
 
-            arrayHelper.addInBigArray(testValue[0] , testValue[1] , testValue[2])
+**/
+        let testValue = arrayHelper.addVehicle();
 
+        arrayHelper.addInBigArray(testValue[0] , testValue[1] , testValue[2]);
 /**
             console.log(globalArray[65][0]);
         console.log(globalArray[65][1]);
@@ -36,82 +44,68 @@ class arrayHelper{
         console.log(globalArray[65][0]);
         console.log(globalArray[65][1]);
         console.log(globalArray[65][2]);
-
 **/
-
     }
-
 
     static addInBigArray(marqueVehicle, plaqueVehicle ,colorVehicle)
     {
         for(let i = 0 ; i <= globalArray.length - 1 ; i++)
         {
-
             if (globalArray[i][0] =="" &&  globalArray[i][1] ==  "" )
             {
-
            //     console.log(`il reste une place à ${i}`)
-
                 globalArray[i][0] = marqueVehicle;
                 globalArray[i][1] = plaqueVehicle;
                 globalArray[i][2] = colorVehicle;
 
-                console.log(`Véhicule ajouté à ${i}   ${globalArray[i][0]}   ${  globalArray[i][1]}   ${globalArray[i][2]}`)
-
+                console.log(`Véhicule ajouté à ${i}   ${globalArray[i][0]}   ${  globalArray[i][1]}   ${globalArray[i][2]}`);
                break;
             }
-
             else
-                {
-                    console.log(`Cette place est déjà prise ${i} et ${globalArray[i][0]} ${globalArray[i][1]} ${globalArray[i][2]}`)
-
+            {
+                console.log(`Cette place est déjà prise ${i} et ${globalArray[i][0]} ${globalArray[i][1]} ${globalArray[i][2]}`);
             }
 
         }
     }
 
-    static clearPlaceInArray(place , matricule)
+    static clearPlaceInArray()//marqueVehicle , matricule)
     {
-        for(let i = 0 ; i<= globalArray.length - 1 ; i++)
+        let  brandVehicle = prompt("marqueVehicle");
+        let ID = prompt("matricule");
+        for(let i = 0 ; i <= globalArray.length - 1 ; i++)
         {
-            if (globalArray[i][0] ==place && globalArray[i][1] == matricule)
+            if (globalArray[i][0] == brandVehicle &&  globalArray[i][1] ==  ID)
             {
-
                 globalArray[i][0] = "";
                 globalArray[i][1] = "";
                 globalArray[i][2] = "";
-
+                console.log(`Véhicule supprimé !`);
+                break;
             }
-
             else
-                {
-                alert(`Le véhicule n'a pas pu être trouvé !`);
+            {
+                console.log(`Cette place est déjà prise ${i} e`);
             }
-
         }
-
-
     }
-    static encoderVehicle()
+
+    static addVehicle()
     {
-        let marqueVehicle = "";
-        let taille = "";
+        let brandVehicle = "";
+        let heightVehicle = "";
         let colorVehicle = "";
-        let plaqueVehicle = "";
-
-        while(marqueVehicle == "" || taille >= MAX_HEIGHT ||  colorVehicle == "" || plaqueVehicle =="" ||  marqueVehicle.includes(" ")  ||
-        colorVehicle.includes(" ")|| plaqueVehicle.includes(" ") || taille =="" ||  taille ==" ")
+        let IDVehicle = "";
+        while(brandVehicle == "" ||  brandVehicle.includes(" ") || heightVehicle >= MAX_HEIGHT ||  colorVehicle == "" || IDVehicle ==""  ||
+        colorVehicle.includes(" ")|| IDVehicle.includes(" ") || heightVehicle =="" ||  heightVehicle ==" ")
         {
-            marqueVehicle = prompt("Veuillez insèrer la marque de votre véhicule")
-            taille = prompt("Veuillez insèrer la taille de votre véhicule")
-            colorVehicle = prompt("Veuillez insèrer la couleur de votre véhicule")
-            plaqueVehicle = prompt("Veuillez insèrer la plaque de votre véhicule")
+            brandVehicle = prompt("Veuillez insèrer la marque de votre véhicule");
+            heightVehicle = prompt("Veuillez insèrer la taille de votre véhicule");
+            colorVehicle = prompt("Veuillez insèrer la couleur de votre véhicule");
+            IDVehicle = prompt("Veuillez insèrer la plaque de votre véhicule");
 
         }
-
-        return [marqueVehicle ,colorVehicle , plaqueVehicle]
-
+        return [brandVehicle ,IDVehicle , colorVehicle];
     }
-
 }
 
---
+++ JS/functionsWithoutStatic.js
@@ -0,0 +1,104 @@
+let globalArray1 = new Array(70 );
+//const MAX_HEIGHT = 3.00
+function initializeArray()
+    {
+        for(let i = 0 ; i <= globalArray1.length - 1 ; i++)
+        {
+
+            globalArray1[i] = new Array(3);
+
+            for(let o = 0 ; o <= globalArray1[i].length - 1 ; o++)
+            {
+                globalArray1[i][o] = "";
+            }
+
+        }
+    }
+
+function resetArray()
+    {
+        globalArray1 = new Array(70);
+    }
+
+function beginWithThisFunction()
+    {
+        if(globalArray1.every( elem => elem == undefined) == true){initializeArray();}
+        let testValue = addVehicle();
+        addInBigArray(testValue[0] , testValue[1] , testValue[2]);
+
+        /**
+         console.log(globalArray[65][0]);
+         console.log(globalArray[65][1]);
+         console.log(globalArray[65][2]);
+
+         globalArray[65][0] = "Volvo";
+         globalArray[65][1] = "1-AAA-000";
+         globalArray[65][2] = "BLACK";
+         console.log(globalArray[65][0]);
+         console.log(globalArray[65][1]);
+         console.log(globalArray[65][2]);
+
+         **/
+    }
+
+function addInBigArray(marqueVehicle, plaqueVehicle ,colorVehicle)
+    {
+        for(let i = 0 ; i <= globalArray1.length - 1 ; i++)
+        {
+            if (globalArray1[i][0] =="" &&  globalArray1[i][1] ==  "" )
+            {
+                globalArray1[i][0] = marqueVehicle;
+                globalArray1[i][1] = plaqueVehicle;
+                globalArray1[i][2] = colorVehicle;
+                console.log(`Véhicule ajouté à ${i}   ${globalArray1[i][0]}   ${  globalArray1[i][1]}   ${globalArray1[i][2]}`)
+                break;
+            }
+            else
+            {
+                console.log(`Cette place est déjà prise ${i} et ${globalArray1[i][0]} ${globalArray1[i][1]} ${globalArray1[i][2]}`)
+            }
+
+        }
+    }
+
+
+function clearPlaceInArray()//marqueVehicle , matricule)
+    {
+        let brandVehicle = prompt("marqueVehicle");
+        let ID = prompt("matricule");
+        for(let i = 0 ; i <= globalArray1.length - 1 ; i++)
+        {
+            if (globalArray1[i][0] == brandVehicle &&  globalArray1[i][1] ==  ID)
+            {
+                globalArray1[i][0] = "";
+                globalArray1[i][1] = "";
+                globalArray1[i][2] = "";
+                console.log(`Véhicule supprimé !`);
+                break;
+            }
+            else
+            {
+                console.log(`Cette place est déjà prise ${i} e`);
+
+            }
+        }
+    }
+
+function addVehicle()
+    {
+        let brandVehicle = "";
+        let heightVehicle = "";
+        let colorVehicle = "";
+        let IDVehicle = "";
+        while(brandVehicle == "" ||  brandVehicle.includes(" ") || heightVehicle >= MAX_HEIGHT ||  colorVehicle == "" || IDVehicle ==""  ||
+        colorVehicle.includes(" ")|| IDVehicle.includes(" ") || heightVehicle =="" ||  heightVehicle ==" ")
+        {
+            brandVehicle = prompt("Veuillez insèrer la marque de votre véhicule");
+            heightVehicle = prompt("Veuillez insèrer la taille de votre véhicule");
+            colorVehicle = prompt("Veuillez insèrer la couleur de votre véhicule");
+            IDVehicle = prompt("Veuillez insèrer la plaque de votre véhicule");
+
+        }
+        return [brandVehicle ,IDVehicle , colorVehicle];
+    }
+
